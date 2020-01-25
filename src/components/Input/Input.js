08cfb3f06d585ee5d92147854f7c22b6d49d9b56
konstantinpodmasko <podmasko.konstantin@gmail.com>
2020-01-25 19:54:50 +0300

    implemented Input onChange handling

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Input.css';
 
-function Input() {
-  return <input className="input input--focus" />;
+function Input({ onChange, value: valueProp }) {
+  const [inputValue, setInputValue] = useState(valueProp);
+
+  function handleChange(event) {
+    const { value } = event.target;
+
+    setInputValue(value);
+    onChange(value);
+  }
+
+  useEffect(
+    function() {
+      setInputValue(valueProp);
+    },
+    [valueProp]
+  );
+
+  return (
+    <input
+      className="input input--focus"
+      onChange={handleChange}
+      value={inputValue}
+    />
+  );
 }
 
 export default Input;
