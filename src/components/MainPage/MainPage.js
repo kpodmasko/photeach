8c46c1ade3d114f018812845805901a99e8716f2
konstantinpodmasko <podmasko.konstantin@gmail.com>
2020-01-25 16:41:32 +0300

    refactored and added footer in MainPage

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { faTextWidth, faPhotoVideo } from '@fortawesome/free-solid-svg-icons';
 import Page from '../Page';
-import Icon from '../Icon';
 import Group from '../Group';
+import FooterLink from '../FooterLink';
+import IconLink from '../IconLink';
 
 const description = `
         Данное приложение позволяет искать слова по фотографии.\n
         Для этого требуется ввести слово и загрузить фотографию.\n
     `;
 
+function MainPageFooter() {
+  return <FooterLink to="/result">ПОИСК</FooterLink>;
+}
+
 function MainPage() {
   return (
-    <Page description={description}>
+    <Page description={description} footer={<MainPageFooter />}>
       <Group vertical>
-        <Icon icon={faTextWidth} link="/search" />
-        <Icon icon={faPhotoVideo} link="/photo" />
+        <IconLink icon={faTextWidth} to="/search" />
+        <IconLink icon={faPhotoVideo} to="/photo" />
       </Group>
     </Page>
   );
