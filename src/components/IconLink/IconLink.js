ab001e19980dfe548010b13f0278c286b767897b
konstantinpodmasko <podmasko.konstantin@gmail.com>
2020-01-25 18:44:10 +0300

    implemented iconStyle prop handling in IconLink

--- src/components/IconLink/IconLink.js
+++ src/components/IconLink/IconLink.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Link from '../Link';
 import Icon from '../Icon';
 
-function IconLink({ to, icon }) {
+function IconLink({ to, icon, iconStyle }) {
   return (
     <Link to={to} className="icon_link">
-      <Icon icon={icon} />
+      <Icon icon={icon} style={iconStyle} />
     </Link>
   );
 }
