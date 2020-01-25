92d546cb431794e14e4d6aac573818d9bcad6041
konstantinpodmasko <podmasko.konstantin@gmail.com>
2020-01-25 15:27:03 +0300

    implemented links in icons

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 import './Icon.css';
 
-function Icon({ icon }) {
+function Icon({ icon, link }) {
   return (
     <div className="icon__container icon__container--hover">
-      <FontAwesomeIcon icon={icon} />
+      {link ? (
+        <Link to={link} className="icon__link">
+          <FontAwesomeIcon icon={icon} />
+        </Link>
+      ) : (
+        <FontAwesomeIcon icon={icon} />
+      )}
     </div>
   );
 }
