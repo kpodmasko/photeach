dc046ca714c86520bda4e5c99fc4714b64d4c32e
konstantinpodmasko <podmasko.konstantin@gmail.com>
2020-01-25 18:43:32 +0300

    implemented style prop handling in Icon

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Icon.css';
 
-function Icon({ icon }) {
+function Icon({ icon, style }) {
   return (
-    <div className="icon__container icon__container--hover">
+    <div className="icon__container icon__container--hover" style={style}>
       <FontAwesomeIcon icon={icon} />
     </div>
   );
