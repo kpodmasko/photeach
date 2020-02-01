856edaad5c6f3696df3688e5728e4560d1670699
konstantinpodmasko <podmasko.konstantin@gmail.com>
2020-02-01 16:03:39 +0300

    added disabled prop to links

--- src/components/ApproveLink/ApproveLink.js
+++ src/components/ApproveLink/ApproveLink.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import IconLink from '../IconLink';
 
-function ApproveLink({ to, onClick }) {
+function ApproveLink(props) {
   const iconStyle = {
     borderColor: '#2cb824',
     backgroundColor: '#76de70'
   };
 
-  return (
-    <IconLink iconStyle={iconStyle} to={to} icon={faCheck} onClick={onClick} />
-  );
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <IconLink {...props} icon={faCheck} iconStyle={iconStyle} />;
 }
 
 export default ApproveLink;
--- src/components/IconLink/IconLink.js
+++ src/components/IconLink/IconLink.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Link from '../Link';
 import Icon from '../Icon';
 
-function IconLink({ to, icon, iconStyle, onClick }) {
+function IconLink({ to, icon, iconStyle, onClick, disabled }) {
   return (
-    <Link to={to} className="icon_link" onClick={onClick}>
+    <Link to={to} className="icon_link" onClick={onClick} disabled={disabled}>
       <Icon icon={icon} style={iconStyle} />
     </Link>
   );
