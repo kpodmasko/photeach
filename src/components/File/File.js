c535c2d05e5abb0786534e61cc94d6cbfd83431d
konstantinpodmasko <podmasko.konstantin@gmail.com>
2020-02-01 15:50:04 +0300

    refactored File

--- src/components/File/File.js
+++ src/components/File/File.js
@@ -1,40 +1,22 @@
-import React, { useState, useRef } from 'react';
-import Group from '../Group';
+import React, { useState, useRef, useEffect } from 'react';
 import './File.css';
 
-/**
- * TODO: change from file to image
- * TODO: refactor image config naming
- * TODO: check handleChange state setting
- */
-
-function toBase64(file) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = error => reject(error);
-  });
-}
-
-function File({ onChange, imageConfig }) {
-  const { name, value } = imageConfig;
+function File({ onChange, file: fileProp, label = '', className = '' }) {
   const fileRef = useRef(null);
-  // coma is needed as there is splitting in render
-  const [file, setFile] = useState(value);
-  const [fileName, setFileName] = useState(name);
-  const isOnOnlyShowMode = !onChange;
+  const [file, setFile] = useState(fileProp);
+  const totalClassName = ['file', className].join(' ');
 
-  async function handleChange() {
-    const fileConfig = fileRef.current.files[0];
-    const fileWithBase64 = await toBase64(fileConfig);
+  useEffect(
+    function() {
+      if (file && typeof onChange === 'function') {
+        onChange(file);
+      }
+    },
+    [file]
+  );
 
-    setFileName(fileConfig.name);
-    setFile(fileWithBase64);
-    onChange({
-      image: fileWithBase64,
-      imageName: fileConfig.name
-    });
+  function handleChange() {
+    setFile(fileRef.current.files[0]);
   }
 
   function handleButtonClick(e) {
@@ -45,32 +27,21 @@ function File({ onChange, imageConfig }) {
   }
 
   return (
-    <div className="file__container">
-      <Group>
-        {!isOnOnlyShowMode && (
-          <div className="file__upload_button_container">
-            <button
-              type="button"
-              className="file__upload_button"
-              onClick={handleButtonClick}
-            >
-              Загрузить картинку
-            </button>
-          </div>
-        )}
-        {file.length > 1 && (
-          <div className="file__uploaded_image_container">
-            <img src={file} className="file__uploaded_image" alt={fileName} />
-          </div>
-        )}
-      </Group>
+    <>
+      <button
+        type="button"
+        className={totalClassName}
+        onClick={handleButtonClick}
+      >
+        {label}
+      </button>
       <input
         ref={fileRef}
         type="file"
         onChange={handleChange}
-        className="file__native_item"
+        className="file__origin"
       />
-    </div>
+    </>
   );
 }
 
